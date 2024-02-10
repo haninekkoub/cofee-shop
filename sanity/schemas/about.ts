6b8458827ba494ff0f55f172891037ff5195fae3
haninekkoub <haninekkoub@gmail.com>
2024-02-10 21:01:33 +0100

    feat: component

---
+++ sanity/schemas/about.ts
@@ -0,0 +1,12 @@
+export default {
+    name:"product",
+    type:"object",
+    title:"Product",
+    fields: [
+        {
+            name:"name",
+            type:"string",
+            title: "Product Name"
+        },
+    ]
+}
